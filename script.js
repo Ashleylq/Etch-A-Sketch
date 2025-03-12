0483161d6972b7fd20940c874a5362875f11cd2d
Ashleylq <ironmanashley@gmail.com>
2025-03-12 12:53:05 +0530

    Create flexbox grid

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let btn = document.querySelector(".setSize");
+const container = document.querySelector(".container");
+const btn = document.querySelector(".setSize");
 let gridPixels = 0;
 btn.addEventListener("click", () => {
     let pixel = prompt("How many pixels do you want on the sketchboard?");
     gridPixels = Math.round(Number(pixel));
     if (isNaN(gridPixels) === true || gridPixels < 1 || gridPixels > 100){
         alert("Invalid input. Please enter a number between 1 to 100")
+        return;
+    }
+    if(container.firstChild){
+        while(container.firstChild){
+            container.removeChild(container.lastChild);
+        }
+    }
+    for (let i = 1; i <= gridPixels; i++){
+        let rowContainer = document.createElement("div");
+        rowContainer.className = "rowContainer"
+        container.appendChild(rowContainer);
+        for(let i = 1; i <= gridPixels; i++){
+            let sketchDiv = document.createElement("div");
+            sketchDiv.className = "sketchDiv";
+            rowContainer.appendChild("sketchDiv");
+        }
     }
 })
