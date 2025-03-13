8254ba1f7843a3c3e9d43f0deebe69109d190d06
Ashleylq <ironmanashley@gmail.com>
2025-03-13 12:32:42 +0000

    Add color change when hover

--- script.js
+++ script.js
@@ -2,25 +2,30 @@ const container = document.querySelector(".container");
 const btn = document.querySelector(".setSize");
 let gridPixels = 0;
 btn.addEventListener("click", () => {
-    let pixel = prompt("How many pixels do you want on the sketchboard?");
-    gridPixels = Math.round(Number(pixel));
-    if (isNaN(gridPixels) === true || gridPixels < 1 || gridPixels > 100){
-        alert("Invalid input. Please enter a number between 1 to 100")
-        return;
-    }
-    if(container.firstChild){
-        while(container.firstChild){
+    let pixel = prompt("How many rows and columns do you want on the grid?");
+    gridPixels = Number(pixel);
+    if (isNaN(gridPixels) == false && gridPixels > 0 && gridPixels <= 100){
+        while (container.firstChild){
             container.removeChild(container.lastChild);
         }
+        for (let i = 1 ; i <= gridPixels ; i++){
+            let rowContainer = document.createElement("div");
+            rowContainer.classList.add("rowContainer");
+            container.appendChild(rowContainer);
+            for (let i = 1; i <= gridPixels; i++){
+                let colorDiv = document.createElement("div");
+                colorDiv.classList.add("colorDiv");
+                colorDiv.addEventListener("mouseenter", () => {
+                    colorDiv.classList.add("selected")
+                })
+                colorDiv.addEventListener("mouseleave", () => {
+                    colorDiv.classList.remove("selected")
+                })
+                rowContainer.appendChild(colorDiv);
+            }
+        } 
     }
-    for (let i = 1; i <= gridPixels; i++){
-        let rowContainer = document.createElement("div");
-        rowContainer.className = "rowContainer"
-        container.appendChild(rowContainer);
-        for(let i = 1; i <= gridPixels; i++){
-            let sketchDiv = document.createElement("div");
-            sketchDiv.className = "sketchDiv";
-            rowContainer.appendChild("sketchDiv");
-        }
+    else{
+        alert ("Invalid input. Please enter a number between 1 and 100");
     }
 })
